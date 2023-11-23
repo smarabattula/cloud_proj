4fc86809de9f1658b0af9ef03b3a32b4d6080fef
smaraba <smaraba@ncsu.edu>
2023-11-23 16:57:59 -0500

    simple flask app

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    if request.method == 'POST':
+        # Process the input text (you can add text-to-image generation logic here)
+        input_text = request.form['inputText']
+
+        # For now, let's just pass the input text to the result template
+        return render_template('result.html', input_text=input_text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
